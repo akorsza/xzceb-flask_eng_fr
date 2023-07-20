d3cb34bf3709fd1e2230450b35402b27e98a5bcc
akorsza <anna@cebitec.uni-bielefeld.de>
2023-07-20 15:12:59 -0400

    Final changes

--- final_project/machinetranslation/tests/testy.py
+++ final_project/machinetranslation/tests/testy.py
@@ -1,13 +1,24 @@
 import unittest
-from translator import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 
+#Unit tests for the created translator functions:
 class Test_translated(unittest.TestCase):
-    def test_hello_e2f(self):
+    def test_e2f_equal(self):
         text = 'Hello'
         translated_text = 'Bonjour'
-        self.assertEqual(englishToFrench(text),translated_text)
-    def test_hello_f2e(self):
+        self.assertEqual(english_to_french(text),translated_text)
+
+    def test_e2f_not_equal(self):
+        text = 'Hello'
+        self.assertNotEqual(english_to_french(text),'Hello')
+
+    def test_f2e_equal(self):
         text = 'Bonjour'
         translated_text = 'Hello'
-        self.assertEqual(frenchToEnglish(text),translated_text)
-        
+        self.assertEqual(french_to_english(text),translated_text)
+    def test_f2e_not_equal(self):
+        text = 'Bonjour'
+        self.assertNotEqual(french_to_english(text),'Bonjour')
+
+if __name__ == '__main__':
+    unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,9 +1,20 @@
 from deep_translator import MyMemoryTranslator
-def english_to_french(englishText):
-    translator = MyMemoryTranslator(source='en', target = 'fr')
-    frenchText = translator.translate(englishText)
-    return frenchText
-def french_to_english(frenchText):
-    translator = MyMemoryTranslator(source='fr', target = 'en')
-    englishText = translator.translate(frenchText)
-    return englishText
+
+#english_text = 'Hello world, welcome to python'
+#french_text = 'Bonjour le monde, bienvenue en python'
+
+#functions to translate text
+
+def english_to_french(english_text):
+    """
+    Function to translate english text to french one
+    """
+    french_text = MyMemoryTranslator(source='english', target='french').translate(english_text)
+    return french_text
+
+def french_to_english(french_text):
+    """
+    Function to translate english text to french one
+    """
+    english_text = MyMemoryTranslator(source='french', target='english').translate(french_text)
+    return english_text
